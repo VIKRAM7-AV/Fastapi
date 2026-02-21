a565599e5b3067d4db25990334fcb6a76f3b876a
VIKRAM <vikramria195@gmail.com>
2026-02-21 22:21:24 +0530

    Get methods

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI
 
-app = FastAPI();
+app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"Hello": "World"};
 
+@app.get("/")
+def recommendation(age: int):
+    if age < 18:
+        return {"recommendation": "You are a minor. We recommend you to watch cartoons."}
+    elif 18 <= age < 30:
+        return {"recommendation": "You are a young adult. We recommend you to watch action movies."}
+    elif 30 <= age < 50:
+        return {"recommendation": "You are an adult. We recommend you to watch drama movies."}
+    else:
+        return {"recommendation": "You are a senior. We recommend you to watch classic movies."}
