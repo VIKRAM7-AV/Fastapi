fc4419d696f76d2f835781a15c73e9abdf002966
VIKRAM <vikramria195@gmail.com>
2026-02-24 16:12:45 +0530

    Post Methods - Json Format

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# class User(BaseModel):
+#     name: str
+#     age: int
+#     email:str
 
-@app.get("/")
-def recommendation(age: int):
-    if age < 18:
-        return {"recommendation": "You are a minor. We recommend you to watch cartoons."}
-    elif 18 <= age < 30:
-        return {"recommendation": "You are a young adult. We recommend you to watch action movies."}
-    elif 30 <= age < 50:
-        return {"recommendation": "You are an adult. We recommend you to watch drama movies."}
-    else:
-        return {"recommendation": "You are a senior. We recommend you to watch classic movies."}
+# class UserResponse(BaseModel):
+#     user:User
+
+# @app.get("/")
+# def recommendation(age: int):
+#     if age < 18:
+#         return {"recommendation": "You are a minor. We recommend you to watch cartoons."}
+#     elif 18 <= age < 30:
+#         return {"recommendation": "You are a young adult. We recommend you to watch action movies."}
+#     elif 30 <= age < 50:
+#         return {"recommendation": "You are an adult. We recommend you to watch drama movies."}
+#     else:
+#         return {"recommendation": "You are a senior. We recommend you to watch classic movies."}
+
+
+# @app.post("/user/", response_model=UserResponse)
+# def create_user(user: User):
+#     return {"user": user}
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool 
+
+@app.post("/items/")
+def create_item(item: Item):
+    return {
+        "type": "json",
+        "Name":item.name,
+        "price":item.price,
+        "Offer":item.is_offer
+    }
